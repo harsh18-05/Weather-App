aa4acf4a5f70fc69b1dbf0f78bbf9af4d489453d
harsh rajwanshi <harshrajwanshi8@gmail.com>
2023-12-13 15:10:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+const inputBox = document.querySelector(".input-box");
+const searchBtn = document.getElementById("searchBtn");
+const weather_img = document.querySelector(".weather-img");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const humidity = document.getElementById("humidity");
+const wind_speed = document.getElementById("wind-speed");
+
+const location_not_found = document.querySelector(".location-not-found");
+
+const weather_body = document.querySelector(".weather-body");
+
+async function checkWeather(city) {
+  const api_key = "607b01d5d339e5b3d71ac756bf9fd1cc";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  const weather_data = await fetch(`${url}`).then((response) =>
+    response.json()
+  );
+
+  if (weather_data.cod === `404`) {
+    location_not_found.style.display = "flex";
+    weather_body.style.display = "none";
+    console.log("error");
+    return;
+  }
+
+  console.log("run");
+  location_not_found.style.display = "none";
+  weather_body.style.display = "flex";
+  temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+  description.innerHTML = `${weather_data.weather[0].description}`;
+
+  humidity.innerHTML = `${weather_data.main.humidity}%`;
+  wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+  switch (weather_data.weather[0].main) {
+    case "Clouds":
+      weather_img.src = "/assets/cloud.png";
+      break;
+    case "Clear Sky":
+      weather_img.src = "/assets/clear.png";
+      break;
+    case "Rain":
+      weather_img.src = "/assets/rain.png";
+      break;
+    case "Mist":
+      weather_img.src = "/assets/mist.png";
+      break;
+    case "Snow":
+      weather_img.src = "";
+      break;
+  }
+
+  console.log(weather_data);
+}
+
+inputBox.addEventListener("keyup", (e) => {
+  e.preventDefault();
+  if (e.keyCode === 13) {
+    checkWeather(inputBox.value);
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+});
